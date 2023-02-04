c68b631cbd2f5863a4fb1a9ed1ac24e3ae161eba
MaySuKyiNaing <maysukyinaing@gmail.com>
2023-02-04 23:13:04 +0630

    multi directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
 
+export enum Fruit{
+  Apple = 'Apple',
+  Lemon = 'Lemon',
+  Avogado = 'Avogado',
+  Cherries = 'Cherries'
+}
+
+interface IfFruit{
+  id:number,
+  name:Fruit
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +19,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'multi-structure-directive';
+
+  selectedFruit!:Fruit
+  fruits:string[]=Object.values(Fruit)
+  MAX_BUCKET_LENGTH=10
+  bucket:IfFruit[]=[]
+
+  addSelectedFruitToBucket() {
+    this.bucket.push(
+      {
+        id:Date.now(),
+        name:this.selectedFruit
+      }
+    )
+  }
+
+  deleteFromBucket(item:IfFruit) {
+    this.bucket =
+      this.bucket.filter(fruit => fruit.id != item.id )
+  }
+
 }
